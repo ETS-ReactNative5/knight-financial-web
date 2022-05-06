65df4699ca420f832b599f7ecd0da5fabab449a1
Technical-Arsh <arshad.khan@himotechglobal.com>
2022-05-06 13:55:22 +0530

    first commit

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -54,25 +54,72 @@ class Home extends Component{
 			<section id="banner-sec">
 					<div className="container">
 						<div className="row">
-							<div className="col-lg-7">
+							<div className="col-lg-12">
 							<div className="banner-content">
 								<span>Welcome to</span>
-								<h3>Shoot For The <br></br>Moon At KNIGHT</h3>
-								<p>Trade, Earn, & Raid To Stack Your Riches While Securely Storing Them Within Our Castle Vaults
-								</p>
-								<div id="shiva">Total Riches Stored<span class="count"> 200</span></div>
-								<div className="btn-banner">
-									<a href="#" className="buy-now">Connect Wallet</a>
-									<a href="#">Trade Now</a>
+								<h3>Preeminent Defi Platform<br/>on the BNB Chain</h3>
 								</div>
-							</div>
-							</div>
-							<div className="col-lg-5">
+
+								{/* 
+								<div className="row">
+									<div className="col-md-2">
+									<div className="bag-1">
+									<h6>$0.54</h6>
+									<p>$KNIGHT Price</p>
+								</div>
+									</div>
+									<div className="col-md-2">
+									<div className="bag-1">
+									<h6>$1.2M</h6>
+									<p>$Total Liquidity</p>
+								</div>
+									</div>
+									<div className="col-md-2">
+									<div className="bag-1">
+									<h6>$5M</h6>
+									<p>Total Value</p>
+								</div>
+									</div>
+									<div className="col-md-2">
+									<div className="bag-1">
+									<h6>97</h6>
+									<p>Total Pairs</p>
+								</div>
+									</div>
+								</div> */}
+								<div className="wrp-trade-total">
+									<div className="trade-totalbox">
+										<h3>$0.54</h3>
+										<p>$KNIGHT Price</p>
+									</div>
+									<div className="trade-totalbox">
+										<h3>$1.2M</h3>
+										<p>$Total Liquidity</p>
+									</div>
+									<div className="trade-totalbox">
+										<h3>$5M</h3>
+										<p>Total Value</p>
+									</div>
+									<div className="trade-totalbox">
+										<h3>97</h3>
+										<p>Total Pairs</p>
+									</div>
+								</div>
+
+								{/* <div id="shiva">Total Riches Stored<span class="count"> 200</span></div> */}
+								<div className="btn-banner">
+									<a href="#" className="buy-now">Buy KNIGHT</a>
+									<a href="#">Learn More</a>
+								</div>
+							
+						
+							{/* <div className="col-lg-5">
 								<div className="top-cractor-img">
 									<img src={topcharactor} alt="topcharactor" />
 								</div> 
-							</div>
+							</div> */}
 						</div>
+					</div>
 					</div>
 				</section>
 				<section id="million-sec">
@@ -80,7 +127,7 @@ class Home extends Component{
 						<div className="million-content">
 							<img src={logoicon} />
 							<h3>
-								How To Stack Those Riches 
+							Providing tools for reliable and sustainable yields
 							</h3>
 							
 						</div>
@@ -90,8 +137,8 @@ class Home extends Component{
 								<div className="million-box">
 									<img src={user} />
 									<h3>Trade</h3>
-									<span>Trade between tokens on BSC network in seconds</span>
-									<p>Enjoy the highest liquidity trades with the lowest fees and no registration needed. No hassle & more money for you</p>
+									{/* <span>Trade between tokens on BSC network in seconds</span> */}
+									<p>Enjoy the highest liquidity trades and lowest fees with no registration needed. No hassle & more money for you. </p>
 								</div>
 								</a>
 							</div>
@@ -99,10 +146,9 @@ class Home extends Component{
 								<a href="#">
 								<div className="million-box">
 									<img src={Trades} />
-									<h3>Farms & Market </h3>
-									<span>KNIGHT helps you gain passive income through farming & staking</span>
-									<p>Deposit Partner Tokens in the farm To Start Recruiting More Knights To join your cause. 
-									Stake your KNIGHT in the various Markets to unlock passive income streams
+									<h3>Farms</h3>
+									{/* <span>KNIGHT helps you gain passive income through farming & staking</span> */}
+									<p>Provide KNIGHT LP tokens to our farms and start earning $KNIGHT tokens & trading fees.
 									</p>
 								</div>
 								</a>
@@ -111,9 +157,9 @@ class Home extends Component{
 								<a href="#">
 								<div className="million-box">
 									<img src={Staked} />
-									<h3>Raid</h3>
-									<span>Use Your KNIGHT To Battle Against Other Knights in Games To Win More Riches</span>
-									<p>Provably Fair & Fun Games That Will Challenge Your Knighthood & Allow You To Win Millions In Prizes
+									<h3>Pools</h3>
+									{/* <span>Use Your KNIGHT To Battle Against Other Knights in Games To Win More Riches</span> */}
+									<p>Stake your $KNIGHT tokens to earn more $KNIGHT in $KNIGHT Pool
 									</p>
 								</div>
 								</a>
@@ -122,9 +168,9 @@ class Home extends Component{
 								<a href="#">
 								<div className="million-box">
 									<img src={Staked} />
-									<h3>Castle</h3>
-									<span>KStore Your Riches Within The Castle Treasury & Watch Them Grow</span>
-									<p>An Autocompounding Spell Has Been Cast On The Treasury To Help Greatly Accelerate Your Accumulation Of Riches
+									<h3>Raids</h3>
+									{/* <span>KStore Your Riches Within The Castle Treasury & Watch Them Grow</span> */}
+									<p>Stake your $KNIGHT tokens to earn partner tokens in Raids.
 									</p>
 								</div>
 								</a>
@@ -132,18 +178,18 @@ class Home extends Component{
 						</div>
 						<div className="trade-section">
 							<a href="#">
-							<div className="trade-content">
+							{/* <div className="trade-content">
 								<h3>Earn Passive <br></br> Income With Crypto
 								</h3>
 								<p>Farms to yield KNIGHT 
 									KNIGHT to open up passive income <br></br> stream 
 									Store your riches in the Castle & watch the yield<br></br> compound
-									</p>
+									</p> 
 								<div class="btn-trades">
 									<a href="#" class="trade-now">Start Earning Now</a>
 									<a href="#">Learn</a>
 								</div>
-							</div>
+							</div> */}
 							</a>
 						</div>
 					</div>
@@ -154,20 +200,20 @@ class Home extends Component{
 					<div className="container">
 						
 						<div className="row">
-							<div className="col-lg-6">
+							{/* <div className="col-lg-6">
 								<div className="token-img">
 									<img src={tokens} />
 								</div>
 								
-							</div>
-							<div className="col-lg-6">
+							</div> */}
+							<div className="col-lg-12">
 								<div className="passive-content">
-									<h3>A KNIGHT can do more than slay dragons & drink mead
+									<h3>Crypto’s More Fun & Profitable With Friends
 									</h3>
-									<p>KNIGHT token is the core of the $WIZARD ecosystem. Anything is possible with a great magician by your side.</p>
+									<p>Looking to LAUNCH or have a project on BNB Chain? Join the alliance with KnightSwap & unlock farms and pools <br /> as an added benefit for your community. </p>
 									<div class="btn-explore">
-										<a href="#" class="explore-now">Buy KNIGHT</a>
-										<a href="#">Learn more</a>
+										<a href="#" class="explore-now">Apply To Join</a>
+										<a href="#">Alliance Info</a>
 									</div>
 								</div>
 							</div>
@@ -178,15 +224,14 @@ class Home extends Component{
 				<section id="lightpaper">
 					<div className="container">
 						<div className="head-lightpaper">
-							<h3>No Hassle, only</h3>
-							<p>Trade any token on Binance Smart Chain Easily & Safely.<br></br>
-								Audited & Backed By Trusted Partners. <br></br>
-								Simply connect your wallet and start trading now
+							<h3>knight makes our world <br /> go round.</h3>
+							<p>KNIGHT token is at the heart of the KNIGHT ecosystem. Buy it, win it, farm it,<br></br>
+							spend it, stake it... heck, you can even vote with it!
 							</p>
 						</div>
 						<div className="row">
 							<div className="col-lg-12">
-								<div className="wrp-trade-total">
+								{/* <div className="wrp-trade-total">
 									<div className="trade-totalbox">
 										<h3>$10.78</h3>
 										<p>$SHUSHI Price</p>
@@ -203,7 +248,7 @@ class Home extends Component{
 										<h3>2,111</h3>
 										<p>Total Pairs</p>
 									</div>
-								</div>
+								</div> */}
 								<div className="lightpaper-content">
 										<div className="btn-lightpaper">
 											<a href="#" className="trade-now" target="_blank">Trade Now</a>
